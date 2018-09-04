83c1ff3776a5208d96d24cf2c183ebbc681fbba0
yellowb <122517167@qq.com>
2018-09-04 11:02:40 +0800

    Add connection test class

--- src/main/java/base_usage/EnsureConnection.java
+++ src/main/java/base_usage/EnsureConnection.java
@@ -11,7 +11,7 @@ public class EnsureConnection {
     public static void main(String[] args) {
         RedisClient client = RedisClient.create("redis://localhost:6379");
         StatefulRedisConnection<String, String> connection = client.connect();
-        RedisStringCommands sync = connection.sync();
+        RedisStringCommands<String, String> sync = connection.sync();
         sync.set("key1", "value1");
         Object value = sync.get("key1");
         System.out.println(value.toString());
