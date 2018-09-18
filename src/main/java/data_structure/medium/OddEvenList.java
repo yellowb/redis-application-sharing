8a755732fe56224ac59928ab73e7c35fe1279288
yellowb <122517167@qq.com>
2018-09-18 17:31:15 +0800

    add ppt

--- src/main/java/data_structure/medium/OddEvenList.java
+++ src/main/java/data_structure/medium/OddEvenList.java
@@ -49,8 +49,31 @@ public class OddEvenList {
         return oddHead;
     }
 
+    public static void printList(ListNode head) {
+        while(head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("NULL");
+    }
+
     public static void main(String[] args) {
-        
+        ListNode node_1 = new ListNode(1);
+        ListNode node_2 = new ListNode(2);
+        ListNode node_3 = new ListNode(3);
+        ListNode node_4 = new ListNode(4);
+        ListNode node_5 = new ListNode(5);
+
+        node_1.next = node_2;
+        node_2.next = node_3;
+        node_3.next = node_4;
+        node_4.next = node_5;
+
+        printList(node_1);
+        printList(new OddEvenList().oddEvenList(node_1));
+
+
+
     }
 
 }
