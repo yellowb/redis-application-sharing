b67291f6aa5921039d53019dee47315ece7d8afd
yellowb <122517167@qq.com>
2018-09-19 09:57:06 +0800

    add ppt

--- src/main/java/data_structure/medium/OddEvenList.java
+++ src/main/java/data_structure/medium/OddEvenList.java
@@ -19,30 +19,30 @@ public class OddEvenList {
             return head;
         }
 
-        ListNode oddHead = head;
-        ListNode oddCurr = oddHead;
+        ListNode oddHead = head;  // 指向奇数节点的头部
+        ListNode oddCurr = oddHead;  // 指向每一轮迭代最后一个奇数节点
 
-        ListNode evenHead = head.next;
-        ListNode evenCurr = evenHead;
+        ListNode evenHead = head.next;  // 指向偶数节点的头部
+        ListNode evenCurr = evenHead;  // 指向每一轮迭代最后一个偶数节点
 
+        // 每一轮迭代都交替把oddCurr.next和evenCurr.next指向下一个奇数和偶数节点
         while (true) {
             if (evenCurr.next != null) {
                 oddCurr.next = evenCurr.next;
                 oddCurr = oddCurr.next;
-            }
-            else {
+            } else {
                 break;
             }
 
             if (oddCurr.next != null) {
                 evenCurr.next = oddCurr.next;
                 evenCurr = evenCurr.next;
-            }
-            else {
+            } else {
                 break;
             }
         }
 
+        // 最后把奇数链表的尾巴跟偶数链表的头部连接起来
         evenCurr.next = null;
         oddCurr.next = evenHead;
 
@@ -50,7 +50,7 @@ public class OddEvenList {
     }
 
     public static void printList(ListNode head) {
-        while(head != null) {
+        while (head != null) {
             System.out.print(head.val + " -> ");
             head = head.next;
         }
@@ -63,16 +63,16 @@ public class OddEvenList {
         ListNode node_3 = new ListNode(3);
         ListNode node_4 = new ListNode(4);
         ListNode node_5 = new ListNode(5);
+        ListNode node_6 = new ListNode(6);
 
         node_1.next = node_2;
         node_2.next = node_3;
         node_3.next = node_4;
         node_4.next = node_5;
+        node_5.next = node_6;
 
         printList(node_1);
         printList(new OddEvenList().oddEvenList(node_1));
-
-
 
     }
 
